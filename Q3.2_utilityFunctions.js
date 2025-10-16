82d66218d9cb4cef4cf74e4cb69e134c900ce013
rchumpon <vinzen_victor@hotmail.com>
2025-10-17 01:41:10 +1100

    Create Q3.2_utilityFunctions.js

---
+++ Q3.2_utilityFunctions.js
@@ -0,0 +1,17 @@
+// File: Q3.2_utilityFunctions.js
+// Description: Use the utility functions to find out information
+// Author: Roongroj Chumpon
+// Date: 16/10/25
+
+// Declare a constant variable named 'myString' and assign it a string value.
+const myString = "This is a string"
+
+// Use the .length property to find out the length of myString.
+console.log(`The length of myString is ${myString.length}`);
+
+// Use bracket notation to find out the first character of myString.
+console.log(`The first character of myString is ${myString[0]}`);
+
+// Use bracket notation to find out the 11th character of my String.
+console.log(`The 11th character of myString is ${myString[10]}`);
+

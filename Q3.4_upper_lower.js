63bc4318d7d8d9b8a44c1666e3f6934e748377f5
rchumpon <vinzen_victor@hotmail.com>
2025-10-17 03:06:33 +1100

    Create Q3.4_upper_lower.js

---
+++ Q3.4_upper_lower.js
@@ -0,0 +1,12 @@
+// File: Q3.4_upper_lower.js
+// Description: Change the case of the myName string.
+// Author: Roongroj Chumpon
+// Date: 17/10/25
+
+// Declare a variable.
+const myName = "Roongroj";
+
+// Convert the string stored in myName to uppercase letter.
+console.log(myName.toUpperCase());
+// Convert the string stored in myName to lowercase letter.
+console.log(myName.toLowerCase());

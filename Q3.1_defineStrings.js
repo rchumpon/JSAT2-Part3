ac55776dba109c50f4a55e2233d4b9b5118e623c
rchumpon <vinzen_victor@hotmail.com>
2025-10-16 22:34:53 +1100

    Create Q3.1_defineStrings.js

---
+++ Q3.1_defineStrings.js
@@ -0,0 +1,16 @@
+// File: Q3.1_defineStrings.js
+// Description: Define strings
+// Author: Roongroj Chumpon
+// Date: 16/10/25
+
+const mySting = "This is a string"
+const anotherString = "   Another string"
+const hello = "Hello there!"
+const myName = "Roongroj"
+
+console.log(mySting);
+console.log(anotherString);
+console.log(hello);
+console.log(myName);
+
+
